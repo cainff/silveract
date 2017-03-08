4d24fe441ef14a27c273d84236a23073b837fff9
GeorgesMarie <georgesmariemah@yahoo.fr>
2017-03-08 13:50:52 +0100

    Germain

---
+++ js/silver.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    console.log('je suis pret');
+    $('#phone-contact').museenter().animate({
+        'width': '382px'
+    }, 'slow');
+});
+
+
+
+
+/*
+    $('html, body').animate({scrollTop:0}, 'slow');
+*/
+/*
+    #phone-contact{
+    top: 90%;
+    left: 70%;
+    z-index: 2;
+    bottom: 30%;
+    width: 382px;
+    height: 80px;
+    position: absolute;
+    border: 1px solid #f17f32;
+    background-color: #f17f32;
+    border-top-left-radius: 100px;
+    border-bottom-left-radius: 100px;
+    border-bottom: 3px solid #c26628;
+}
+img[src="../img/call.png"]{
+    width: 15%;
+    padding-top: 12px;
+    padding-left: 12px;
+}
+#number{
+    color: #fff;
+    left: 100px;
+    bottom: 25px;
+    font-size: 30px;
+    position: absolute;
+    font-family: 'Open sans', sans-serif;
+}
+*/
